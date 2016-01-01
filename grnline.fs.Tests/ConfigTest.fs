795875894fb4565c02a0a93f6f0935ee9ef0b912
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-01-02 08:03:40 +0900

    Add Config type annotation
    `obj` type is awful!!! :cold_sweat:

--- grnline.fs.Tests/ConfigTest.fs
+++ grnline.fs.Tests/ConfigTest.fs
@@ -5,8 +5,8 @@ open Type
 
 module ConfigTest =
   let config_path = "C:\\groonga-5.1.0-x64\\groonga-5.1.0-x64\\bin\\groonga.exe"
-  let config = {
-    Config.Path = config_path;
+  let config: Config = {
+    Path = config_path;
     DBPath = "test.db";
     DBEncoding = "UTF-8";
     Pretty = true
