c7d089526dae35ad033e842a5c34a88e734573e2
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-01-03 14:44:17 +0900

    Remove needless lines in ps1

--- RunTests.ps1
+++ RunTests.ps1
@@ -1,6 +1,4 @@
-.\.nuget\NuGet.exe Install Persimmon.Console -Pre -OutputDirectory packages -ExcludeVersion
-
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe grnline.fs.sln /property:Configuration=Debug /property:VisualStudioVersion=12.0 /target:rebuild
+MSBuild.exe grnline.fs.sln /property:Configuration=Debug /property:VisualStudioVersion=12.0 /target:rebuild
 
 $inputs = @(
   ".\grnline.fs.Tests\bin\Debug\grnline.fs.Tests.dll"
