62ef0809ba0a64c9a60a3328dce9aed4a49f145e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-01-03 23:45:34 +0900

    Remove needless lines

--- RunTests.sh
+++ RunTests.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-mono ./.nuget/NuGet.exe Install Persimmon.Console -Pre -OutputDirectory packages -ExcludeVersion
-
 xbuild grnline.fs.sln /property:Configuration=Debug /property:VisualStudioVersion=12.0 /target:rebuild
 
 inputs="./grnline.fs.Tests/bin/Debug/grnline.fs.Tests.dll"
